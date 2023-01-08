c25ee5be9d4270b7f53d633e2129c9f035b4886c
hadavi <peymanhc@gmail.com>
2023-01-08 22:36:46 +0330

    :up: (translate): add translate

---
+++ src/translate/index.js
@@ -0,0 +1,10 @@
+// application
+import enMessages from './messages/en';
+import arMessages from './messages/ar';
+import frMessages from './messages/fr';
+
+export default {
+    en: enMessages,
+	ar: arMessages,
+    fr: frMessages,
+};
