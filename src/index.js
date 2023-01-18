2913568d982e1e0542045bfb5dce5e29814c0a4d
hadavi <peymanhc@gmail.com>
2023-01-18 22:31:03 +0330

    :up: (images): add index

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
+serviceWorker.register();
